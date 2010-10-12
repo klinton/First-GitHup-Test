99f07d7b2e6f6e71ab8ab93297fb45d8a012abec
Klinton <klinton.lee@gmail.com>
2010-10-12 16:24:54 -0700

    logo helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
   # Return a title on a per-page bisis.
+  def logo
+   image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
+
   def title
     base_title = "Ruby on Rails SampleApp"
     if @title.nil?
