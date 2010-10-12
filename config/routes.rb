b741951f52ee4889a19e16c3e757af555e912a9b
Klinton <klinton.lee@gmail.com>
2010-10-12 11:58:32 -0700

    Set Default Rails page to welcome.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 SampleApp::Application.routes.draw do
 
-  get "pages/help"
-
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
+  match 'help', :to => 'pages#help'
+  match 'home', :to => 'pages#home'
+  match 'contact', :to => 'pages#contact'
+  match 'about', :to => 'pages#about'
 
 
   # The priority is based upon order of creation:
@@ -58,7 +55,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
